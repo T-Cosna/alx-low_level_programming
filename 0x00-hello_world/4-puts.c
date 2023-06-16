dd57b8ed3825c913e3a63eb7f89f48111a509ee0
T-Cosna <tshiamo2252@gmail.com>
2023-06-15 19:25:52 -0700

    HelloWorld

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
-#include <stdio.h>
-int main(void)/*the entry point*/
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return(0);/*return 0*/
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,12 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main(void) /*entry point*/
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0; /*return 0*/
+	return (0);
 }
